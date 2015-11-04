23f80c0ae851a9b40c174ba78f07186cb9b488bb
Charles Moulliard <ch007m@gmail.com>
2015-11-04 07:22:59 +0100

    Merge backend server with feedhenry server project

--- backend-service/application.js
+++ backend-service/application.js
@@ -2,6 +2,10 @@ var mbaasApi = require('fh-mbaas-api');
 var express = require('express');
 var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var methodOverride  = require('method-override');
+
+var blogService = require('./lib/articles');
 
 // list the endpoints which you want to make securable here
 var securableEndpoints;
@@ -10,6 +14,13 @@ securableEndpoints = ['/hello'];
 // fhlint-end
 
 var app = express();
+
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(methodOverride());      // simulate DELETE and PUT
 
 // Enable CORS for all requests
 app.use(cors());
@@ -26,6 +37,10 @@ app.use(mbaasExpress.fhmiddleware());
 
 // fhlint-begin: custom-routes
 app.use('/hello', require('./lib/hello.js')());
+app.get('/articles', blogService.findAll);
+app.get('/articles/:id', blogService.findById);
+app.get('/articles/searchuser/:user', blogService.findByUser)
+app.post('/articles', blogService.newPost);
 // fhlint-end
 
 // Important that this is last!
